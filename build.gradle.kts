3f235331bbf76c0b2cc967c926efbb8ffa1abc0b
Carlos Goncalves <carlos@Carloss-MacBook-Pro.local>
2025-08-14 14:43:20 +0100

    Add repo settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,15 @@ dependencyResolutionManagement {
         }
     }
 }
+
+repositories {
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/LRNcardozoWDF/mendra-sdk-publisher")
+        credentials {
+            username = System.getenv("GITHUB_ACTOR") ?: "USERNAME_PLACEHOLDER"
+            password = System.getenv("GITHUB_TOKEN") ?: "TOKEN_PLACEHOLDER"
+        }
+    }
+    mavenCentral()
+}
